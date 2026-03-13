a744503fb98178e26f05e4d535cf56481e379747
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-13 07:05:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
 const cacheName = 'kiot-hub-v1';
+// እዚህ ጋር ያሉትን የፋይል ስሞች በ GitHub ካለህ ስም ጋር አንድ መሆናቸውን አረጋግጥ
 const assets = [
   './',
-  './index.html'
+  './index.html',
+  './manifest.json',
+  './maths.pdf',
+  './cpp.pdf',
+  './english2.pdf',
+  './history.pdf',
+  './civics.pdf',
+  './anthropology.pdf',
+  './emerging.pdf'
 ];
 
+// ፋይሎቹን በስልኩ ሜሞሪ ላይ ለመጫን (Install)
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      console.log('Caching assets...');
+      return cache.addAll(assets);
     })
   );
 });
 
+// ዳታ በሌለበት ሰዓት ከሜሞሪው አውጥቶ እንዲያሳይ (Fetch)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
