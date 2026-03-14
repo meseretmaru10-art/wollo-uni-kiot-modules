e1bf6a876f2ab82d275e96ef608e654ebd6d2b80
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-14 19:19:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = 'kiot-hub-final';
+const CACHE_NAME = 'kiot-hub-v5';
 const assets = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'icon.png',
-  'maths.pdf',
-  'cpp.pdf',
-  'english2.pdf',
-  'history.pdf',
-  'civics.pdf',
-  'anthropology.pdf',
-  'emerging.pdf'
+  '/wollo-uni-kiot-modules/',
+  '/wollo-uni-kiot-modules/index.html',
+  '/wollo-uni-kiot-modules/manifest.json',
+  '/wollo-uni-kiot-modules/sw.js'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
