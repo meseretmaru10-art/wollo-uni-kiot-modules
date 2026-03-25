d0f055b477f040473f4f5684e1db84faaac2ef5a
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-25 15:09:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'kiot-hub-v17';
+const assets = ['./', './index.html', './manifest.json', './icon.png', './maths.pdf', './cpp.pdf', './english2.pdf', './history.pdf', './civics.pdf', './anthropology.pdf', './emerging.pdf'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
