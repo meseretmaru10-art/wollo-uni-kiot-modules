77fbd1d5869e50983ae94df727b361ea85efc410
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-25 10:05:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
-const CACHE_NAME = 'kiot-hub-v3';
-
-// ያለ ዳታ እንዲከፈቱ የምንፈልጋቸው ፋይሎች ዝርዝር
+const CACHE_NAME = 'kiot-hub-v5';
 const assets = [
   './',
   './index.html',
   './manifest.json',
   './icon.png',
-  // PDF ፋይሎች እዚህ ተጨምረዋል
   './maths.pdf',
   './cpp.pdf',
   './english2.pdf',
@@ -16,32 +13,23 @@ const assets = [
   './emerging.pdf'
 ];
 
-// ፋይሎቹን በስልኩ ካሽ (Cache) ውስጥ መጫን
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('PDFs and Assets are being cached for offline use...');
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
-// አሮጌ ካሽ ካለ አዲሱን ስሪት ለማስገባት ማጽዳት
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
 });
 
-// መረጃን ከካሽ ውስጥ መጥራት
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
