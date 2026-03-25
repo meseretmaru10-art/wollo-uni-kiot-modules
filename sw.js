9a5dc24202b9389221de8b05488bf821fb89eb1d
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-25 22:33:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kiot-hub-v25'; // ካሸውን ለማደስ ቁጥሩን ቀይሬዋለሁ
+const CACHE_NAME = 'kiot-hub-v40'; // አዲስ ስሪት በሆን ቁጥር ይህን ቁጥር ቀይረው
 const assets = [
   './',
   './index.html',
@@ -10,4 +10,24 @@ const assets = [
   './history.pdf',
   './civics.pdf',
   './anthropology.pdf',
-  './emerging.pdf
+  './emerging.pdf'
+];
+
+// አፑ መጀመሪያ ሲጫን ፋይሎቹን ወደ ስልክህ ሜሞሪ (Cache) ይጭናል
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// ዳታ በሌለበት ጊዜ ከስልክህ ሜሞሪ ፋይሎቹን ያነባል
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
