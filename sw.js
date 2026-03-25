49c57eef19264d17b91a441403bcc83035b90eac
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-25 12:07:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kiot-hub-v10';
+const CACHE_NAME = 'kiot-hub-v11'; // ቨርዥኑን ወደ 11 ቀይረነዋል
 const assets = [
   './',
   './index.html',
@@ -13,15 +13,33 @@ const assets = [
   './emerging.pdf'
 ];
 
+// ፋይሎቹን ሴቭ ለማድረግ
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching all assets...');
+      return cache.addAll(assets);
+    })
   );
 });
 
+// የቆየ ካሽ ካለ ለማጽዳት
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// ኢንተርኔት በሌለበት ጊዜ ከሴቭ የተደረገው እንዲያነብ
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
