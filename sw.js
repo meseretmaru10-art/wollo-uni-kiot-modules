7c4d47e306df8649de6e43de451854afb04a0c82
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-04-09 15:22:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,21 @@
-const CACHE_NAME = 'kiot-hub-v25';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
-  './maths.pdf',
-  './cpp.pdf',
-  './english2.pdf',
-  './history.pdf',
-  './civics.pdf',
-  './anthropology.pdf',
-  './emerging.pdf'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching assets...');
-      return cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })));
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
-});
+{
+  "name": "KIOT Freshman Hub",
+  "short_name": "KIOT Hub",
+  "description": "Wollo University KIOT Freshman Modules and GPA Calculator",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#456d80",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
